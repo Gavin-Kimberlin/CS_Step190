f6cb16f0637f65fa7ece241683f9d5c95c7cb22a
Gavin Kimberlin <guviner@gmail.com>
2020-07-27 23:17:17 -0700

    Create a program using generic parameters.

--- CS_Step190/Program.cs
+++ CS_Step190/Program.cs
@@ -10,6 +10,25 @@ namespace CS_Step190
     {
         static void Main(string[] args)
         {
+            Employee<string> employee1 = new Employee<string>();
+            employee1.Things = new List<string>()
+            { "OneThing", "TwoThing", "ThreeThing", "FourThing" };
+
+            Employee<int> employee2 = new Employee<int>();
+            employee2.Things = new List<int>()
+            { 80085, 69420, 12345, 8675309};
+
+            for (int i = 0; i < employee1.Things.Count(); i++)
+            {
+                Console.WriteLine("\n" + employee1.Things[i]);
+            }
+
+            for (int i = 0; i < employee2.Things.Count(); i++)
+            {
+                Console.WriteLine("\n" + employee2.Things[i]);
+            }
+
+            Console.ReadLine();
         }
     }
 }
